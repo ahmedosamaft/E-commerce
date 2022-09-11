27593ee8f425c166e4c1dd5660509df76506bbb2
Ahmed Osama <androidbasha572@gmail.com>
2022-09-11 21:16:55 +0200

    Updates

---
+++ precache-manifest.de8fb1f40b56a2fc47cf9ba8ae3e050c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e25daa642b96b8f4ee1",
+    "url": "/E-commerce/static/css/main.a71b6906.chunk.css"
+  },
+  {
+    "revision": "0e25daa642b96b8f4ee1",
+    "url": "/E-commerce/static/js/main.0e25daa6.chunk.js"
+  },
+  {
+    "revision": "795ef40dc50792254af0",
+    "url": "/E-commerce/static/css/1.1dc7be4e.chunk.css"
+  },
+  {
+    "revision": "795ef40dc50792254af0",
+    "url": "/E-commerce/static/js/1.795ef40d.chunk.js"
+  },
+  {
+    "revision": "21fb6efbb46058665a5e",
+    "url": "/E-commerce/static/js/runtime~main.21fb6efb.js"
+  },
+  {
+    "revision": "86910fb192e7d5b52d36e7f5b4bf81a3",
+    "url": "/E-commerce/index.html"
+  }
+];
